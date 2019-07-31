ff9f6c84ef0a9e0a82a26eee2670e09c89aedd3c
Chrisando Ryan <chrisando.siahaan@gmail.com>
2019-07-31 16:15:12 +0700

    initial commit

---
+++ MainScene.js
@@ -0,0 +1,132 @@
+var MainScene = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+        function MainScene () {
+            Phaser.Scene.call(this, { key: 'MainScene' });
+        },
+    createArk: async function () {
+        this.sys.DialogModalPlugin.init();
+        this.sys.DialogModalPlugin.setText('Great, let\'s create new Ark for you. Any cool name? ', true);
+        let arkname = await waitInput();
+        this.sys.DialogModalPlugin.setText(arkname + '? Cool enough. Now tell me its Attack Point (AP).', true);
+        let attackpoint = await waitInput();
+        this.sys.DialogModalPlugin.setText('What about its Hitpoint (HP)?', true);
+        let hitpoint = await waitInput();
+        this.sys.DialogModalPlugin.setText('Amazing. Last, any special skill? ', true);
+        let skill = await waitInput();
+        this.sys.DialogModalPlugin.setText('Done. Uploading Ark data to the server.. Please wait.', true);
+        await sleep(3000);
+
+        request('/ark/create', {
+            ark_name: arkname,
+            attack: attackpoint,
+            skill: skill,
+            hitpoint: hitpoint,
+            token: localStorage.getItem('jwt')
+        })
+        .done(async (e) => {
+            this.sys.DialogModalPlugin.setText('Badass! Your new Ark has been delivered to the Hangar. ', true);
+            await sleep(2000);
+        })
+        .fail((e) => {
+            this.sys.DialogModalPlugin.setText('Error. Message: ' + e.responseText, true);
+        });
+    },
+    accessHangar: function () {
+
+    },
+    preload: function () {
+		this.load.scenePlugin('DialogModalPlugin', 'dialog_plugin.js');
+    },
+    create: function () {
+        // create your world here
+        // this.add.image(400, 300, 'bg');
+        this.add.bitmapText(160, 100, 'main_font', 'Main Menu', 28);
+
+        this.add.text(160, 135, 'Profile: Administrator (3b27)', {
+            fill: '#fff',
+            fontWeight: 'bold',
+            fontSize: '15px'
+        });
+
+        const createArkButton = this.add.text(160, 200, '[ Build New ARK ]', {
+            fill: '#0f0',
+            fontSize: '20px'
+        });
+
+        createArkButton.setInteractive()
+            .on('pointerdown', () => {
+                this.createArk();
+            })
+            .on('pointerover', () => {
+                createArkButton.setStyle({
+                    fill: '#ff0'
+                });
+            })
+            .on('pointerout', () => {
+                createArkButton.setStyle({
+                    fill: '#0f0'
+                });
+            });
+
+        const battleButton = this.add.text(160, 230, '[ Showdown Arena ]', {
+            fill: '#0f0',
+            fontSize: '20px'
+        });
+
+        battleButton.setInteractive()
+            .on('pointerdown', () => {
+            })
+            .on('pointerover', () => {
+                battleButton.setStyle({
+                    fill: '#ff0'
+                });
+            })
+            .on('pointerout', () => {
+                battleButton.setStyle({
+                    fill: '#0f0'
+                });
+            });
+
+        const hangarButton = this.add.text(160, 260, '[ Access Hangar ]', {
+            fill: '#0f0',
+            fontSize: '20px'
+        });
+
+        hangarButton.setInteractive()
+            .on('pointerdown', () => {
+                game.scene.stop(this);
+				game.scene.start('HangarScene');
+            })
+            .on('pointerover', () => {
+                hangarButton.setStyle({
+                    fill: '#ff0'
+                });
+            })
+            .on('pointerout', () => {
+                hangarButton.setStyle({
+                    fill: '#0f0'
+                });
+            });
+
+        const logoutButton = this.add.text(160, 320, '[ Logout ]', {
+            fill: '#0f0',
+            fontSize: '20px'
+        });
+
+        logoutButton.setInteractive()
+            .on('pointerdown', () => {
+                
+            })
+            .on('pointerover', () => {
+                logoutButton.setStyle({
+                    fill: '#ff0'
+                });
+            })
+            .on('pointerout', () => {
+                logoutButton.setStyle({
+                    fill: '#0f0'
+                });
+            });
+    }
+});
---
+++ index_copy.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>ArknadO - The Game</title>
+	<script src="//cdn.jsdelivr.net/npm/phaser@3.18.1/dist/phaser.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<style media='screen' type='text/css'>
+	@font-face {
+		font-family: main_font;
+		src: url('assets/font/font.ttf');
+		font-weight: 400;
+		font-weight: bold;
+	}
+</style>
+
+<body>
+	<div id="game"></div>
+	<form id="keyboard">
+		<input type="text" name="uinput" id="uinput">
+		<input type="submit" value="Send">
+	</form>
+</body>
+<script>
+	var config = {
+		type: Phaser.AUTO,
+		parent: 'game',
+		width: 800,
+		height: 600,
+		scene: {
+			preload: preload,
+			create: create,
+			update: update
+		}
+	};
+
+	// this is an async timeout util (very useful indeed)
+	const timeout = async ms => new Promise(res => setTimeout(res, ms));
+	let next = false; // this is to be changed on user input
+
+	var game = new Phaser.Game(config);
+	document.getElementById("keyboard").addEventListener('submit', (e) => {
+		e.preventDefault();
+		next = true
+	});
+
+	function request(endpoint, data) {
+		data = JSON.stringify(data);
+		return $.ajax({
+				url: 'http://localhost:8000' + endpoint,
+				dataType: 'json',
+				type: "POST",
+        		data: data,
+				beforeSend: function() { }
+			})
+	}
+
+	async function waitInput() {
+		while (next === false) await timeout(50); // pause script but avoid browser to freeze ;)
+		next = false; // reset var
+		let data = document.getElementById("uinput").value;
+		document.getElementById("uinput").value = "";
+		return data;
+	}
+
+	function preload() {
+		// this.load.image('sky', 'assets/bg/sky.png');
+		// this.load.image('cbg', 'assets/bg/cbg.png');
+		// this.load.image('cfg', 'assets/bg/cfg.png');
+		// this.load.image('btn', 'assets/ui/buttonStock1d.png');
+		this.load.scenePlugin('DialogModalPlugin', 'dialog_plugin.js');
+		this.load.bitmapFont('main_font', 'assets/font/font.png', 'assets/font/font.fnt');
+		//   	this.load.spritesheet('dude', 
+		//        'assets/dude.png',
+		//     { frameWidth: 32, frameHeight: 48 }
+		// );
+	}
+
+	async function login(dialogModal) {
+		dialogModal.init();
+		dialogModal.setText('What\'s your name? ', true);
+		let uname = await waitInput();
+		dialogModal.setText('Cool. Password? ', true);
+		let passwd = await waitInput();
+		dialogModal.setText('Nice! Please wait.. ', true);
+		request('/user/login', {username: uname, password: passwd})
+			.done((e) => {
+				dialogModal.setText('Success! Welcome back. ', true);
+			})
+			.fail((e) => {
+				dialogModal.setText('Error. Message: ' + e.responseText, true); 
+			});
+	}
+
+	async function register(dialogModal) {
+		dialogModal.init();
+		dialogModal.setText('New guy! How may I call you? ', true);
+		let uname = await waitInput();
+		dialogModal.setText('Your email? ', true);
+		let email = await waitInput();
+		dialogModal.setText('Password? (please make it secure) ', true);
+		let passwd = await waitInput();
+		dialogModal.setText('Splendid. Creating your account.. ', true);
+		request('/user/register', {username: uname, email: email, password: passwd})
+			.done((e) => {
+				dialogModal.setText('All clear. Welcome aboard, Cap. ', true);
+			})
+			.fail((e) => {
+				dialogModal.setText('Error. Message: ' + e.responseText, true); 
+			});
+	}
+
+	function create() {
+		// let sky = this.add.image(400, 300, 'sky');
+		// let cbg = this.add.image(400, 300, 'cbg');
+		// let cfg = this.add.image(400, 300, 'cfg');
+
+		this.add.bitmapText(160, 100, 'main_font', 'ArknadO II', 32);
+		this.add.text(160, 140, 'The Dawn of Ark Brutality Of The Shark Tornado', {
+			fill: '#0f0'
+		});
+
+		const loginButton = this.add.text(160, 200, 'Login', {
+			fill: '#0f0'
+		});
+		loginButton.setInteractive()
+			.on('pointerdown', () => {
+				login(this.sys.DialogModalPlugin);
+				// this.sys.DialogModalPlugin.init()
+				// this.sys.DialogModalPlugin.setText('What\'s your name? ', true);
+				// // var name = prompt("Username");
+				// await waitInput();
+				// this.sys.DialogModalPlugin.setText('Password? ', true);
+				// var pass = prompt("Password");
+				// this.input.keyboard.on('keydown', (e) => { this.sys.DialogModalPlugin.getInput(e) }, this);
+			})
+			.on('pointerover', () => {
+				loginButton.setStyle({
+					fill: '#ff0'
+				});
+			})
+			.on('pointerout', () => {
+				loginButton.setStyle({
+					fill: '#0f0'
+				});
+			});
+
+		const regisButton = this.add.text(160, 230, 'Register', {
+			fill: '#0f0'
+		});
+		regisButton.setInteractive()
+			.on('pointerdown', () => {
+				register(this.sys.DialogModalPlugin);
+			})
+			.on('pointerover', () => {
+				regisButton.setStyle({
+					fill: '#ff0'
+				});
+			})
+			.on('pointerout', () => {
+				regisButton.setStyle({
+					fill: '#0f0'
+				});
+			});
+
+		const exitButton = this.add.text(160, 280, 'Exit', {
+			fill: '#0f0'
+		})
+		exitButton.setInteractive().on('pointerdown', () => {
+				this.scene.start('MainScene')
+			})
+			.on('pointerover', () => {
+				exitButton.setStyle({
+					fill: '#ff0'
+				});
+			})
+			.on('pointerout', () => {
+				exitButton.setStyle({
+					fill: '#0f0'
+				});
+			});
+
+		// this.add.text(50, 520, 'For Playstation 3 - Playstation 2 - Nintendo Switch - Windows XP/Vista', {
+		// 	fill: '#fff'
+		// });
+	}
+
+	function update() {
+
+	}
+	
+</script>
+
+</html>
